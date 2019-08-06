2fec96d34748cb9e17d8dda150d7cb75717147d0
J K <kim.ju652@gmail.com>
2019-08-06 22:47:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,12 +2,15 @@
 
 def load_library
   # code goes here
+  require "yaml"
+  emoticons = YAML.load_file('emoticons.yml')
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(english)
   # code goes here
+  
 end
 
-def get_english_meaning
+def get_english_meaning(japanese)
   # code goes here
 end
